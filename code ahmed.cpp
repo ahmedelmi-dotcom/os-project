04110e7d1febcb420c9c2eb10acb27f1f330dd38
ahmedelmi-dotcom <63073789+ahmedelmi-dotcom@users.noreply.github.com>
2020-04-03 21:07:12 +0530

    Add files via upload

---
+++ code ahmed.cpp
@@ -0,0 +1,93 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int bt[10],p[10],n,temp,i,j,wt[10],sum=0,at[10],tat[10];
+    float avg;
+   
+    printf("Enter total no of proces:");
+    scanf("%d",&n);
+     printf("enter arrival time of process");
+
+     for(i=0;i<n;i++)
+    {
+        printf("\n arrival time of process P%d:",i);
+        scanf("%d",&bt[i]);
+        p[i]=i;
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[i]>bt[j])
+            {
+                temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
+        }
+    }
+     
+    printf("\n Enter burst time for each process:-");
+    for(i=0;i<n;i++)
+    {
+        printf("\nBurst time of process P%d:",i);
+        scanf("%d",&bt[i]);
+        p[i]=i;
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[i]>bt[j])
+            {
+                temp=bt[i];
+                bt[i]=bt[j];
+                bt[j]=temp;
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+            }
+        }
+    }
+    wt[0]=0;
+    for(i=1;i<n;i++)
+    {
+        wt[i]=wt[i-1]+bt[i-1];
+    }
+    for(i=0;i<n;i++)
+    {
+        sum+=wt[i];
+    }
+    avg=(float)sum/n;
+    printf("\n Waiting time for each process:-");
+    for(i=0;i<n;i++)
+    {
+        printf("\n Waiting time for process P%d is %d sec.",p[i],wt[i]);
+       
+    }
+    printf("\n Average waiting time is %f sec.",avg);
+
+    tat[0]=0;
+    for (i=1;i<n;i++)
+    {
+    	tat[i]=wt[i]+bt[i];
+    }
+    for(i=0;i<n;i++)
+    {
+    	sum+=tat[i];
+    }
+    avg=(float)sum/n;
+    printf("\n turn around time for each process:-");
+    for (i=0;i<n;i++)
+    {
+    	printf("\n turn around time for process p%d is %d sec.",p[i],tat[i]);
+    }
+    printf("\n average turn around time is %f sec.",avg);
+    getch();
+    return 0;
+}
